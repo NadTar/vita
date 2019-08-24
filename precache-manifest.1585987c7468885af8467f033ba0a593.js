3231139360bb930ff03f0c93314855311b5b157a
Nadir Tareen <nadirkt@uw.edu>
2019-08-23 22:30:03 -0700

    Updates

---
+++ precache-manifest.1585987c7468885af8467f033ba0a593.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10f675cd7a2b9b3caaa3220faf45302",
+    "url": "/vita/index.html"
+  },
+  {
+    "revision": "d2f07265fa823a170f0c",
+    "url": "/vita/static/css/main.6ee833e9.chunk.css"
+  },
+  {
+    "revision": "a7d0ce105b338436309c",
+    "url": "/vita/static/js/2.0c5ceee3.chunk.js"
+  },
+  {
+    "revision": "d2f07265fa823a170f0c",
+    "url": "/vita/static/js/main.c18dac6c.chunk.js"
+  },
+  {
+    "revision": "9ac0be0d82063bea4b00",
+    "url": "/vita/static/js/runtime~main.b9bbd0a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vita/static/media/logo.5d5d9eef.svg"
+  }
+]);
